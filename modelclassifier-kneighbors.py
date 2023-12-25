0935097c417397ad09633b4b6a80eac2eab2e6d0
rahman21_ <sidqi.rahman@gmail.com>
2023-12-25 21:02:03 +0700

    fixing code kneighbors

--- modelclassifier-kneighbors.py
+++ modelclassifier-kneighbors.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
@@ -25,7 +26,7 @@ y = data["booking_status"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Pelatihan model (contoh menggunakan RandomForestClassifier)
-model = RandomForestClassifier()
+model = KNeighborsClassifier()
 model.fit(X_train, y_train)
 
 # Evaluasi model
